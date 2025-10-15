8003627339893560e72d97d5a5800b2f5889c319
Sudheer Thupili <142662209+Sudheer056@users.noreply.github.com>
2025-10-15 19:42:59 +0000

    Final project submission

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -16,7 +16,9 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('/docs', app, document);
 
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+await app.listen(port);
+console.log(`Application is running on: http://localhost:${port}`);
 }
 bootstrap().catch((err) => {
   console.log(err);
